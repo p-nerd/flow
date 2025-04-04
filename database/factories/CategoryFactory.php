f5bd7c50bfaca11593450bdd2259204db92c8a79
Shihab Mahamud <shihab4t@gmail.com>
2025-04-05 04:52:31 +0600

    categories controller update

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->first()->id,
+            'name' => fake()->word().' Account',
+            'plaid_id' => fake()->boolean(70) ? fake()->unique()->regexify('[A-Za-z0-9]{24}') : null,
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => fn (array $attributes) => fake()->dateTimeBetween($attributes['created_at'], 'now'),
+        ];
+    }
+}
