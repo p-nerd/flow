bdaf74c168e8d5456f9d1d469f4d8718df9dd4fc
Shihab Mahamud <shihab4t@gmail.com>
2025-04-05 17:06:32 +0600

    category fixed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -25,4 +25,42 @@ class CategoryFactory extends Factory
             'updated_at' => fn (array $attributes) => fake()->dateTimeBetween($attributes['created_at'], 'now'),
         ];
     }
+
+    /**
+     * Indicate that the account has no Plaid ID.
+     */
+    public function withoutPlaidId(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'plaid_id' => null,
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the account has a Plaid ID.
+     */
+    public function withPlaidId(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'plaid_id' => $this->faker->unique()->regexify('[A-Za-z0-9]{24}'),
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the account belongs to a specific user.
+     */
+    public function forUser(User|int $user): Factory
+    {
+        $userId = $user instanceof User ? $user->id : $user;
+
+        return $this->state(function (array $attributes) use ($userId) {
+            return [
+                'user_id' => $userId,
+            ];
+        });
+    }
 }
